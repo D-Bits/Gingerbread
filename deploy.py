69e2ba531039df9a9cc2201f8f1c1b2a50927fca
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-14 17:11:44 -0700

    Fixed typo

--- deploy.py
+++ deploy.py
@@ -3,6 +3,6 @@ from subprocess import run
 deploy = run(['pyinstaller', 'main.py', '-F', '-n', 'Gingerbread'])
 
 if deploy.returncode == 0:
-    input('Delpoyment succesful. Press enter to exit.')
+    input('Deployment succesful. Press enter to exit.')
 else:
     input('WARNING: Deployment failed. Press enter to exit.')
